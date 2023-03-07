aa44a51daa037472c85a25b95e906ed6bf9ccdfe
Joe Heck <heckj@mac.com>
2023-03-07 08:04:55 -0800

    updating preview script to use package structure and plugin

--- scripts/preview-docs.sh
+++ scripts/preview-docs.sh
@@ -4,16 +4,6 @@ set -eou pipefail
 # see https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 THIS_SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PACKAGE_PATH=$THIS_SCRIPT_DIR/../
-BUILD_DIR=$PACKAGE_PATH/.build
+cd ${PACKAGE_PATH}
 
-swift build \
-    --package-path $PACKAGE_PATH \
-    --target Automerge \
-    -Xswiftc -emit-symbol-graph \
-    -Xswiftc -emit-symbol-graph-dir \
-    -Xswiftc $BUILD_DIR
-xcrun docc preview Automerge.docc \
-    --fallback-display-name Automerge \
-    --fallback-bundle-identifier org.automerge.Automerge \
-    --fallback-bundle-version 1 \
-    --additional-symbol-graph-dir $BUILD_DIR
+swift package --disable-sandbox preview-documentation --target Automerge
