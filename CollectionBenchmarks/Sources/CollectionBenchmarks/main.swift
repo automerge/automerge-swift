83d661f25567b1f27410eae1ebc14d9e839985b9
Joe Heck <heckj@mac.com>
2023-03-09 11:12:11 -0800

    adding notes on making our own generators, after re-aquainting myself with the underlying code

--- CollectionBenchmarks/Sources/CollectionBenchmarks/main.swift
+++ CollectionBenchmarks/Sources/CollectionBenchmarks/main.swift
@@ -12,16 +12,16 @@ import Foundation
 //
 // It's worthwhile to look at existing benchmarks that Karoy created for
 // https://github.com/apple/swift-collections and build from those as bases:
-// - https://github.com/apple/swift-collections/blob/main/Benchmarks/Benchmarks/SetBenchmarks.swift
-// - https://github.com/apple/swift-collections/blob/main/Benchmarks/Benchmarks/OrderedSetBenchmarks.swift
-// - https://github.com/apple/swift-collections/blob/main/Benchmarks/Benchmarks/DictionaryBenchmarks.swift
+// - https://github.com/apple/swift-collections/blob/main/Benchmarks/Sources/Benchmarks/SetBenchmarks.swift
+// - https://github.com/apple/swift-collections/blob/main/Benchmarks/Sources/Benchmarks/OrderedSetBenchmarks.swift
+// - https://github.com/apple/swift-collections/blob/main/Benchmarks/Sources/Benchmarks/DictionaryBenchmarks.swift
 //
 // There are 4 different default
 // 'input generators' registered and immediately available:
 //
-// Int.self
-// [Int].self
-// ([Int], [Int]).self
+// [Int.self](https://github.com/apple/swift-collections-benchmark/blob/main/Sources/CollectionsBenchmark/Benchmark/Benchmark.swift#L24-L26)
+// [[Int].self](https://github.com/apple/swift-collections-benchmark/blob/main/Sources/CollectionsBenchmark/Benchmark/Benchmark.swift#L27-L29)
+// [([Int], [Int]).self](https://github.com/apple/swift-collections-benchmark/blob/main/Sources/CollectionsBenchmark/Benchmark/Benchmark.swift#L30-L32)
 // Insertions.self
 //
 // The first three result in an array of length 'size' with integers, in shuffled order.
@@ -30,7 +30,20 @@ import Foundation
 // testing random insertions.
 //
 // You can create your own input generators for more custom interactions or
-// patterns that you wish to explore.
+// patterns that you wish to explore. To do so, use `benchmark.registerInputGenerator(for:)`.
+// The for: variable defines the type that you're returning to the benchmark, and the function
+// takes a trailing closure that is provided the size (of type `Int`) to the closure, and expects
+// the type you defined in the `for:` parameter to be returned for that instance of size.
+//
+// For example, the definition of the `[Int].self` generator is:
+// 
+// ```swift
+// registerInputGenerator(for: [Int].self) { size in
+//     (0 ..< size).shuffled()
+// }
+// ```
+// The above generator creates a range of values from 0 to the size provided and returns 
+// them as an array of integers, shuffled.
 
 var benchmark = Benchmark(title: "Automerge")
 
