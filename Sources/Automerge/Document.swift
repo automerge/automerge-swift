f0457159f2f12982149b49f9c5251db99574eced
Joe Heck <heckj@mac.com>
2024-01-19 12:18:13 -0800

    updating docs to call out implementation details and rewrite into slightly simpler sentence structures to make it easier to parse

--- Sources/Automerge/Document.swift
+++ Sources/Automerge/Document.swift
@@ -854,11 +854,16 @@ public final class Document: @unchecked Sendable {
     /// The number of change hashes in the returned set represents the number of concurrent changes the document tracks.
     /// The heads returned are the tips of the change graph managed by Automerge, so the number of heads is the number
     /// of concurrent changes at the tips of the graph.
-    /// For example, if two peers make a change concurrently and sync with each other then the synced document will have
-    /// two heads, but as soon as one of them makes a change on top of the synced document it will return to one head,
-    /// because the new change is not concurrent with the previous changes but causally succeeding both of them.
-    /// In many ways Automerge heads are analogous to git heads in that the head hashes identify commits in a graph of
-    /// commits, the difference with git is that the heads can identify multiple points in the graph.
+    ///
+    /// For example, if two peers make a change concurrently and sync with each other then the synced document will have two heads.
+    /// As soon as one of them makes a change on top of the synced document it will return to one head,
+    /// because the new change is not concurrent with the previous changes but causally succeeds them.
+    ///
+    /// In many ways `heads` returned by Automerge are analogous to heads in `git`
+    /// in that the hashes returned identify commits in a graph of commits.
+    /// The difference from git is that in git the heads can identify multiple points in the graph.
+    ///
+    /// Implementation details:
     ///
     /// The number of hashes in the document does increase linearly with the number of changes, but Automerge doesn't
     /// encode the hashes into the output of that is provided when you invoke ``save()``.
