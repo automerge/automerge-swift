915b5953cf07d1642fcf31f3c19972a2243c3099
Joe Heck <heckj@mac.com>
2024-05-20 13:49:10 -0700

    loose stubbing, maybe wrong path

--- rust/src/span.rs
+++ rust/src/span.rs
@@ -1,16 +1,29 @@
+use crate::ScalarValue;
 use automerge as am;
-use std::sync::Arc;
+use std::collections::HashMap;
 
-use crate::mark
 // ?? not entirely clear on how to expose MarkSet - nothing in the existing API exposes
 // a map-like type through uniFFI - it's all been enums, lists, and structs so far.
 // in am, MarkSet uses BTreeMap, which is a standard collection type, indexed with smolStr
 
-
 pub enum Span {
     /// A span of text and the marks that were active for that span
-    Text(String, Option<Arc<MarkSet>>),
+    Text(TextValue),
     /// A block marker
-    Block(am::hydrate::Map)
+    Block(MapValue),
 }
 
+pub enum AMValue {
+    Scalar(ScalarValue),
+    Map(MapValue),
+    List(Vec<AMValue>),
+    Text(TextValue),
+}
+
+pub struct MapValue {
+    value: HashMap<String, AMValue>
+}
+pub struct TextValue {
+    value: String,
+    marks: HashMap<String, ScalarValue>,
+}
