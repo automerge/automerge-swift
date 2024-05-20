e3ddd966d9521d23fba1d973c8215b23684d5727
Joe Heck <heckj@mac.com>
2024-05-20 14:07:25 -0700

    WIP refining to exposed types in Swift - or ideas for them

--- rust/src/span.rs
+++ rust/src/span.rs
@@ -1,3 +1,4 @@
+use crate::Mark;
 use crate::ScalarValue;
 use automerge as am;
 use std::collections::HashMap;
@@ -6,6 +7,12 @@ use std::collections::HashMap;
 // a map-like type through uniFFI - it's all been enums, lists, and structs so far.
 // in am, MarkSet uses BTreeMap, which is a standard collection type, indexed with smolStr
 
+pub struct SpanThing {
+    pub parents: Vec<String>, // as Vec<AMValue> to convert through hydrate?
+    pub r#type: String,
+    pub attr: HashMap<String, AMValue>, // as MapValue
+}
+// maps to am::iter::Span
 pub enum Span {
     /// A span of text and the marks that were active for that span
     Text(TextValue),
@@ -13,6 +20,7 @@ pub enum Span {
     Block(MapValue),
 }
 
+// maps to am::hydrate::Value
 pub enum AMValue {
     Scalar(ScalarValue),
     Map(MapValue),
@@ -20,10 +28,12 @@ pub enum AMValue {
     Text(TextValue),
 }
 
+// maps to am::hydrate::Map
 pub struct MapValue {
-    value: HashMap<String, AMValue>
+    value: HashMap<String, AMValue>,
 }
+// maps to am::hydrate::Text
 pub struct TextValue {
     value: String,
-    marks: HashMap<String, ScalarValue>,
+    marks: Vec<Mark>,
 }
