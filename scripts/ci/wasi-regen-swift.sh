6d5715f260fd0086a7858d3362ddaccbe683e62f
Joe Heck <heckj@mac.com>
2024-06-06 15:48:42 -0700

    adding explicit rebuild flow to WASM/WASI testing flow on Linux

---
+++ scripts/ci/wasi-regen-swift.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This script is strictly for CI usage to support pull requests that update
+# the Rust core library. The headers need to be regenerated for the WASI build
+# explicitly, which is part of the release process for general purposes, but
+# may need an explicit one-off to correctly test a local build in CI.
+
+# bash "strict" mode
+# https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
+set -euxo pipefail
+
+THIS_SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+RUST_FOLDER="$THIS_SCRIPT_DIR/../../rust"
+
+FRAMEWORK_NAME="automergeFFI"
+
+SWIFT_FOLDER="$THIS_SCRIPT_DIR/../AutomergeUniffi"
+cargo_build="cargo build --manifest-path ${RUST_FOLDER}/Cargo.toml"
+
+echo "▸ Building for wasm32-wasi"
+$cargo_build --target wasm32-wasi --locked --release
+
+# copies the generated header from AutomergeUniffi/automergeFFI.h to
+# Sources/_CAutomergeUniffi/include/automergeFFI.h within the project
+cp "${SWIFT_FOLDER}/automergeFFI.h" "${SWIFT_FOLDER}/../Sources/_CAutomergeUniffi/include"
+cp "${SWIFT_FOLDER}/automergeFFI.modulemap" "${SWIFT_FOLDER}/../Sources/_CAutomergeUniffi/include/module.modulemap"
